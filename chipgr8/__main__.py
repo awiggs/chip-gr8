e47fd64cef9165e0bf057fc0133a3b3268b5891f
Jonathan <jonbezeau@gmail.com>
2019-06-06 08:35:27 -0700

    Partially implemented the CLI.
    Implemented all of the command line flags. They do not actually call
    relevant functionality yet however. Todo is to link them with associated
    functionality instead of just printing what it wants to do when it
    receives those input flags.

--- chipgr8/__main__.py
+++ chipgr8/__main__.py
@@ -1,4 +1,9 @@
 import chipgr8
+import argparse
+import sys
+
+def romExists(rom):
+    return True
 
 '''
 #TODO 
@@ -12,4 +17,29 @@ usage: chipgr8
     -r --rom     The path or name of a rom
 '''
 
-chipgr8.init()
+
+# Parse command line arguments and pass appropriate parameters to init
+parser = argparse.ArgumentParser(allow_abbrev=False, description="ChipGr8: The AI focused Chip 8 Emulator")
+parser.add_argument("-v", "--version", action="store_true", default=False, dest="version", help="display version information and then exit")
+parser.add_argument("-vvv", "--verbose", action="store_true", default=False, dest="verbosity", help="show more information on the console while running")
+parser.add_argument("-r", "--rom", action="store", dest="rom", default=False, help="provide a chip 8 rom to run")
+parser.add_argument("-a", "--assemble", nargs=2, dest="aInOut", help="Run the assembler with in input SRC and output ROM")
+parser.add_argument("-d", "--disassemble", nargs=2, dest="dInOut", help="Run the disassembler with in input ROM and output SRC")
+
+
+args = parser.parse_args()
+
+if args.version:
+    print("---VERSION INFO---")
+    sys.exit(0)
+if args.rom:
+    if(not romExists(args.rom)):
+        print(args.rom + " is not a valid ROM")
+    else:
+        print("Starting up Chip Gr8 with ", args.rom)
+elif args.aInOut:
+    print("Assemble with:\n\tinputSrcFile: " + args.aInOut[0] + "\n\toutputRomFile: " + args.aInOut[1])
+elif args.dInOut:
+    print("disassemble with:\n\tinputRomFile: " + args.dInOut[0] + "\n\toutputSrcFile: " + args.dInOut[1])
+
+chipgr8.init(args.verbosity, args.rom)
