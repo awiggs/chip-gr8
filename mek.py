afd8fcbd00ffa13995defd07ee291afe4db5e7dd
James <jambar@uvic.ca>
2019-06-06 12:17:05 -0700

    Added std=c99 to mek.py

--- mek.py
+++ mek.py
@@ -11,7 +11,7 @@ cc = gcc_clang(cmd='gcc', dbg='gdb')
 # any libraries to load
 libs = []
 # additional compiler flags
-flags = ['-Wall', '-Werror', '-pedantic']
+flags = ['-Wall', '-Werror', '-pedantic', '-std=c99']
 
 if options.command != command_test:
     linkflags = ['-shared']    
