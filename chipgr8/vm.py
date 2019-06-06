ea883db27fe69db4a04432550bb7af8ef4b0e014
john-curry <liquidsnakeeter@gmail.com>
2019-06-06 13:13:45 -0700

    fixed broken commit. render calls io.py

--- chipgr8/vm.py
+++ chipgr8/vm.py
@@ -1,6 +1,7 @@
 import os
 import pickle as pkl
 import chipgr8.core as core
+import chipgr8.io
 
 class Chip8VM(object):
     '''
@@ -8,17 +9,21 @@ class Chip8VM(object):
     pythonic interface to the VM state.
     '''
     vm = None
+    window = None
 
     def __init__(
         self,
-        display   = None,
-        timing    = None,
+        display   = False,
+        timing    = False,
     ):
         '''
         Initializes a new Chip8VM object, calling core.initVM to allocate a new
         C struct.
         '''
         self.vm = core.initVM()
+
+        if display:
+            self.window = io.ChipGr8Window() 
 
     def __del__(self):
         '''
@@ -115,32 +120,32 @@ class Chip8VM(object):
 
         keymask = bin(0)
 
-        if k0 not None: keymask += bin(k0)
-        if k1 not None: keymask += bin(k1) << 1
-        if k2 not None: keymask += bin(k2) << 2
-        if k3 not None: keymask += bin(k3) << 3
-        if k4 not None: keymask += bin(k4) << 4
-        if k5 not None: keymask += bin(k5) << 5
-        if k6 not None: keymask += bin(k6) << 6
-        if k7 not None: keymask += bin(k7) << 7
-        if k8 not None: keymask += bin(k8) << 8
-        if k9 not None: keymask += bin(k9) << 9
-        if kA not None: keymask += bin(kA) << 10
-        if kB not None: keymask += bin(kB) << 11
-        if kC not None: keymask += bin(kC) << 12
-        if kD not None: keymask += bin(kD) << 13
-        if kE not None: keymask += bin(kE) << 14
-        if kF not None: keymask += bin(kF) << 15
+        if not k0 == None: keymask += bin(k0)
+        if not k1 == None: keymask += bin(k1) << 1
+        if not k2 == None: keymask += bin(k2) << 2
+        if not k3 == None: keymask += bin(k3) << 3
+        if not k4 == None: keymask += bin(k4) << 4
+        if not k5 == None: keymask += bin(k5) << 5
+        if not k6 == None: keymask += bin(k6) << 6
+        if not k7 == None: keymask += bin(k7) << 7
+        if not k8 == None: keymask += bin(k8) << 8
+        if not k9 == None: keymask += bin(k9) << 9
+        if not kA == None: keymask += bin(kA) << 10
+        if not kB == None: keymask += bin(kB) << 11
+        if not kC == None: keymask += bin(kC) << 12
+        if not kD == None: keymask += bin(kD) << 13
+        if not kE == None: keymask += bin(kE) << 14
+        if not kF == None: keymask += bin(kF) << 15
 
         core.send_input(keymask)
 
 
-    def render():
+    def render(self):
         '''
         Force a render to the window (if it is open).
         '''
-        pass #TODO
-
+        if not self.window == None:
+            self.window.render(self.VRAM(), True) 
     
     # State Methods
 
---
+++ tests/test_start.py
@@ -0,0 +1,5 @@
+from chipgr8.vm import Chip8VM as vm
+
+c = vm()
+
+c.render()
