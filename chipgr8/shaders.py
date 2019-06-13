af6723593dfdde521e15b28e0d5508723acea33f
ejrbuss <ejrbuss@gmail.com>
2019-06-13 10:25:11 -0700

    fixed rainbow shader

--- chipgr8/shaders.py
+++ chipgr8/shaders.py
@@ -5,9 +5,9 @@ def rainbow(screen, x, y):
     frac = y / screen.height
     if frac < 0.5:
         a = frac * 2
-        b = 1 - frac
+        b = 1 - a
         return (255 * b, 255 * a, 0)
     else:
         a = (frac - 0.5) * 2
-        b = 1 - frac
+        b = 1 - a
         return (0, 255 * b, 255 * a)
