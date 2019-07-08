240eb33b5009c430ffe6502d942fb9a3ddf3fb02
ejrbuss <ejrbuss@gmail.com>
2019-07-08 12:16:53 -0700

    implemented Query object

--- chipgr8/__init__.py
+++ chipgr8/__init__.py
@@ -3,6 +3,7 @@ from .chipgr8      import init
 from .disassembler import disassemble, hexdump
 from .assembler    import assemble
 from .util         import findROM
+from .query        import Query
 
 VERSION     = '0.0.1'
 DESCRIPTION = "ChipGr8: The AI focused Chip 8 Emulator"
