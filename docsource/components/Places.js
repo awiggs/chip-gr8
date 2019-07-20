83e0fd771ce74fd7d12aaa3a952aace8ff91ee3b
Andrew Wiggins <xandrewwiggins@gmail.com>
2019-07-19 17:46:42 -0700

    Added API page to website

--- docsource/components/Places.js
+++ docsource/components/Places.js
@@ -1,10 +1,11 @@
 import Link  from 'next/link';
 import Vars  from '../Vars'; 
 
-export default ({ home, about, project }) => (
+export default ({ home, about, project, api }) => (
     <>
         <Link href={Vars.sitePrefix + '/index'}><a className={`m-md ${home ? 'active' : ''}`}><i className='fas fa-home' />Home</a></Link>
         <Link href={Vars.sitePrefix + '/project'}><a className={`m-md ${project ? 'active' : ''}`}>Project</a></Link>
+        <Link href={Vars.sitePrefix + '/api'}><a className={`m-md ${api ? 'active' : ''}`}>API</a></Link>
         <Link href={Vars.sitePrefix + '/about'}><a className={`m-md ${about ? 'active' : ''}`}>About</a></Link>
     </>
 );
