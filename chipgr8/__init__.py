d1bcbfd273fa414abcba1eb0b7bdd45e816976b4
ejrbuss <ejrbuss@gmail.com>
2019-07-20 19:41:40 -0700

    all controls except tron

--- chipgr8/__init__.py
+++ chipgr8/__init__.py
@@ -6,6 +6,13 @@ from .util         import findROM, readableInputHistory
 from .query        import Query
 from .observer     import Observer
 from .namedList    import NamedList
+from .games        import (
+    K_NONE,
+    K_0, K_1, K_2, K_3,
+    K_4, K_5, K_6, K_7,
+    K_8, K_9, K_A, K_B,
+    K_C, K_D, K_E, K_F,
+)
 
 themes = {
     'light'    : ('#000000', '#FFFFFF'),
