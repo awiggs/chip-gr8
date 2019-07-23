1407a646281ee7fc62f587ad45fb50cce19e8b9f
Jonathan <jonbezeau@gmail.com>
2019-07-22 17:04:47 -0700

    cave agent progress

--- examples/caveAgent.py
+++ examples/caveAgent.py
@@ -5,47 +5,117 @@ from chipgr8.games import Cave as cave
 
 # Globals
 rate = 1
-downFlag = False
-
+lastMove = None
 vm = chipgr8.init(display=True, ROM=cave.ROM, sampleRate=rate)
+xPos = 0
+yPos = 0
 
-# wait for the game to finish loading
-for _ in range(1000):
-    vm.act(cave.actions.none)
+def start():
+    global rate
+    global lastMove
+    global vm
+    global xPos
+    global yPos
 
-# start the game
-vm.act(cave.actions.start)
+    # wait for the game to finish loading
+    waitToLoad(1000)
 
-#loop actions
-while not vm.done():
+    # start the game
+    vm.act(cave.actions.start)
 
-    # get new observations (x,y) position
-    observations = cave.observe(vm)
-    xPos = observations.myX
-    yPos = observations.myY
+    waitToLoad(1000)
 
-    # get the value of my up,down,left,right
-    try:
-        up          = vm.ctx()[xPos, yPos - 1] == 0 # up is safe
-        right       = vm.ctx()[xPos + 1, yPos] == 0 # right is safe
-        down        = vm.ctx()[xPos, yPos + 1] == 0 # down is safe
-        left        = vm.ctx()[xPos - 1, yPos] == 0 # left is safe
-    except:
-        vm.act(cave.actions.right)
-    # Make decisions
-    if right:
-        vm.act(cave.actions.right)
-        continue
-    if up and not downFlag:
-        vm.act(cave.actions.up)
-        continue
-    if down:
-        vm.act(cave.actions.down)
-        downFlag = True
-        continue
+    #loop actions
+    while not vm.done():
+
+        # get new observations (x,y) position
+        observations = cave.observe(vm)
+        xPos = observations.myX
+        yPos = observations.myY
+
+        # get the value of my up,down,left,right
+        try:
+            up          = vm.ctx()[xPos, yPos - 1] == 0 # up is safe
+            right       = vm.ctx()[xPos + 1, yPos] == 0 # right is safe
+            down        = vm.ctx()[xPos, yPos + 1] == 0 # down is safe
+            left        = vm.ctx()[xPos - 1, yPos] == 0 # left is safe
+        except:
+            move("right")
+            waitToLoad(500)
+            continue
+
+        # Make decisions
+        if left and right and up and down:
+            if lastMove == "left":
+                move("up")
+                continue
+            if lastMove == "right":
+                move("down")
+                continue
+            if lastMove == "up":
+                move("right")
+                continue
+            if lastMove == "down":
+                move("left")
+                continue
+            move("down", False)
+            continue
+        else:
+            if lastMove == "left" and not left:
+                if down:
+                    move("down")
+                    continue
+                else:
+                    move("up")
+                    continue
+            if lastMove == "right" and not right:
+                if up:
+                    move("up")
+                    continue
+                else:
+                    move("down")
+                    continue
+            if lastMove == "up" and not up:
+                if left:
+                    move("left")
+                    continue
+                else:
+                    move("right")
+                    continue
+            if (lastMove == "down" or lastMove == None) and not down:
+                if right:
+                    move("right")
+                    continue
+                else:
+                    move("left")
+                    continue
+        move(lastMove)
+
+
+def move(s, setLM=True):
+    global lastMove
+    
+    if setLM == True:
+        lastMove = s 
+
+    if s == "up":
+        vm.actUntil(cave.actions.up, positionChanged)
+    elif s == "right":
+        vm.actUntil(cave.actions.right, positionChanged)
+    elif s == "down":
+        vm.actUntil(cave.actions.down, positionChanged)
+    elif s == "left":
+        vm.actUntil(cave.actions.left, positionChanged)
     else:
-        downFlag = False
-    if left:
-        vm.act(cave.actions.left)
-        continue
-    vm.act(cave.actions.none)
+        raise Exception("Invalid direction for movement.\nCan only move left/right/up/down")
+
+
+def positionChanged(vm):
+    observations = cave.observe(vm)
+    return xPos != observations.myX or yPos != observations.myY
+
+def waitToLoad(t):
+    for _ in range(t):
+        vm.act(cave.actions.none)
+
+start()
