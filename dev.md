eee839b9280fa8cc839a3b5e4a999d084a7cab25
ejrbuss <ejrbuss@gmail.com>
2019-07-23 23:27:08 -0700

    exporting c functions on windows (hopefully)

--- dev.md
+++ dev.md
@@ -18,9 +18,9 @@ python3 setup.py sdist bdist_wheel
 # Clean dist
 rm -rf ./dist
 
-# Upload to test.pypi (Replace X with version)
-twine upload --repository-url=https://test.pypi.org/legacy/ dist/chipgr8-0.0.X* dist/
+# Upload to test.pypi
+twine upload --repository-url=https://test.pypi.org/legacy/ dist/*
 
-# Upload to pypi (Replace X with version)
-twine upload dist/chipgr8-0.0.X* dist./
+# Upload to pypi
+twine upload dist/*
 ```
--- includes/chip8.h
+++ includes/chip8.h
@@ -13,6 +13,12 @@
 #include "debug.h"
 #include "instructions.h"
 
+#if defined(_WIN32) || defined(_WIN64)
+    #define shared __declspec( dllexport )
+#else
+    #define shared
+#endif
+
 #define PROGRAM_SPACE_START 0x200
 
 #define HEXSPRITE_0         0xF999F
@@ -56,7 +62,7 @@ typedef u16 word_t;
  * @returns a SHARED_LIBRARY_ID to ensure that the library has been loaded
  *          correctly.
  */
-int helloSharedLibrary();
+shared int helloSharedLibrary();
 
 /**
  * Initializes a VM instance to a valid initial sate. This is the only way a 
@@ -65,14 +71,14 @@ int helloSharedLibrary();
  * @params vm   the vm to intialize
  *         freq the frequency as a factor of 60Hz
  */
-void initVM(Chip8VM_t* vm, u8 freq);
+shared void initVM(Chip8VM_t* vm, u8 freq);
 
 /**
  * Steps a VM 1 clock cycle. 
  * 
  * @params vm the vm
  */
-void step(Chip8VM_t* vm);
+shared void step(Chip8VM_t* vm);
 
 /**
  * Fetches the next instruction then increments the program counter by 2.
@@ -108,9 +114,9 @@ void evaluate(Chip8VM_t* vm, Instruction_t inst, word_t opcode);
  *         filePath the path to the ROM
  * @returns         0 on failure
  */
-int loadROM(Chip8VM_t* vm, char* filePath);
+shared int loadROM(Chip8VM_t* vm, char* filePath);
 
-void input(Chip8VM_t*, u16);
+shared void input(Chip8VM_t*, u16);
 
 u8 getRegisterX(word_t opcode);
 
