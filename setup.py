80b405b9b5cfd795e380476359e5f54d8e5bb697
ejrbuss <ejrbuss@gmail.com>
2019-07-23 18:25:34 -0700

    version properly tracked

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from os.path    import join
 from os         import listdir
 from setuptools import setup, Extension, find_packages
-from chipgr8    import VERSION, DESCRIPTION
 
-with open('./README.md') as file:
-    long_description = file.read()
+with open('./chipgr8/VERSION') as f:
+    VERSION = f.read()
+
+with open('./README.md') as f:
+    description = f.read()
 
 libchip_gr8 = Extension(
     name         = 'chipgr8.libchip_gr8',
@@ -15,16 +17,16 @@ libchip_gr8 = Extension(
 setup(
     name                          = 'chipgr8',
     version                       = VERSION,
-    description                   = DESCRIPTION,
-    long_description              = long_description,
+    description                   = 'Chip 8 Emulation for AI',
+    long_description              = description,
     long_description_content_type = 'text/markdown',
-    author                        = 'Chip-Gr8 team',
+    author                        = 'chipgr8',
     author_email                  = 'root@ejrbuss.net',
     license                       = 'MIT',
     url                           = 'https://awiggs.github.io/chip-gr8/',
     include_package_data          = True,
     packages                      = find_packages(),
-    package_data                  = { 'chipgr8': ['data/*/*'] },
+    package_data                  = { 'chipgr8': ['data/*/*', 'VERSION'] },
     ext_modules                   = [libchip_gr8],
     install_requires              = [
         'lazyarray',
