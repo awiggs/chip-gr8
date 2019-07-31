d4cb14dadc95d342c7c3eb2ac313820a53a07ce7
ejrbuss <ejrbuss@gmail.com>
2019-07-31 14:27:08 -0700

    fixed gif styling

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2637ef521d48a3cbcffd";
+/******/ 	var hotCurrentHash = "1c7fcf74c136cea3c690";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
